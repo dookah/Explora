ef052d6db28fbe2cf8e2e82160ed293c6711dff9
Joshua Dean <15590534@students.lincoln.ac.uk>
2019-04-22 17:18:20 +0100

    Correct colour for submit button

--- www/js/main.js
+++ www/js/main.js
@@ -23,7 +23,6 @@ function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8,
-        disableDefaultUI: true,
-        draggable: false
+        disableDefaultUI: true
       });
 }
