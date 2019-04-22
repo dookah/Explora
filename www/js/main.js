174c79c15ef7a688a78d8ec9e947edb581c980ac
Joshua Dean <15590534@students.lincoln.ac.uk>
2019-04-22 14:12:12 +0100

    Added alert

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("deviceready", setup, false);
 
 function setup(){
-    navigator.notification.alert('Winner', alertDismissed, 'Game Over', 'Done');
+    alert("hello");
 }
