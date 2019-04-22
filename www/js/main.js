d66aaf7d5b2f8a478e1f72a23b6efbb34450a2ea
Joshua Dean <15590534@students.lincoln.ac.uk>
2019-04-22 15:41:01 +0100

    First Location attempt

--- www/js/main.js
+++ www/js/main.js
@@ -3,8 +3,24 @@
 document.addEventListener("deviceready", yourCallbackFunction, false);
 
 function yourCallbackFunction(){
-   alert("Hello");
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 };
 
-function alertDismissed() {
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
 };
+
+var map;
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+}
