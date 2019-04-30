9be3ed0647f6649ba48a45edad596ca286aece25
Joshua Dean <15590534@students.lincoln.ac.uk>
2019-04-30 13:07:34 +0100

    Location current

--- www/js/main.js
+++ www/js/main.js
@@ -37,7 +37,7 @@ function deviceReady() {
     if (cordova.platformId == 'android') {
         StatusBar.backgroundColorByHexString("#00C4A7");
     }
-
+    navigator.geolocation.getCurrentPosition(onLocationSuccess, onError);
     //Request device location if the user moves
     navigator.geolocation.watchPosition(onLocationSuccess, onError);
 
